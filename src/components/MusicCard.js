6913e545e1330587b7fd11fe68cea80696000c9b
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-03-12 16:09:26 -0300

    Atende ao requisito 8.

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoadingEnabled: false,
+    };
+  }
+
+  async componentDidMount() {
+    const songList = document.getElementsByClassName('songList');
+    const favoriteSongs = await getFavoriteSongs();
+    for (let index = 0; index < songList.length; index += 1) {
+      for (let index2 = 0; index2 < favoriteSongs.length; index2 += 1) {
+        if (songList[index].value === (favoriteSongs[index2].trackId).toString()) {
+          songList[index].checked = true;
+        }
+      }
+    }
+  }
+
+  async componentDidUpdate() {
+    this.componentDidMount();
+  }
+
   render() {
+    const {
+      isLoadingEnabled,
+    } = this.state;
     const {
       songs,
     } = this.props;
@@ -12,32 +39,54 @@ class MusicCard extends Component {
       collectionName,
     } = albumData;
     return (
-      <div>
-        <p data-testid="artist-name">{ artistName }</p>
-        <p data-testid="album-name">{ collectionName }</p>
-        {
-          songs.map((element, index) => {
-            const {
-              trackName,
-              previewUrl,
-              kind,
-            } = element;
-            return kind === 'song'
-              ? (
-                <div key={ index }>
-                  <p>{trackName}</p>
-                  <audio data-testid="audio-component" src={ previewUrl } controls>
-                    <track kind="captions" />
-                    O seu navegador não suporta o elemento
-                    <code>audio</code>
-                    .
-                  </audio>
-                </div>
-              )
-              : null;
-          })
-        }
-      </div>
+      isLoadingEnabled
+        ? (<p>Carregando...</p>)
+        : (
+          <div>
+            <p data-testid="artist-name">{ artistName }</p>
+            <p data-testid="album-name">{ collectionName }</p>
+            {
+              songs.map((element, index) => {
+                const {
+                  trackName,
+                  previewUrl,
+                  kind,
+                  trackId,
+                } = element;
+                const onClick = async (event) => {
+                  const elementConst = element;
+                  this.setState({ isLoadingEnabled: true });
+                  if (event.target.checked) {
+                    await addSong(elementConst);
+                  } else {
+                    await removeSong(elementConst);
+                  }
+                  this.setState({ isLoadingEnabled: false });
+                };
+                return kind === 'song'
+                  ? (
+                    <div key={ index }>
+                      <p>{trackName}</p>
+                      <audio data-testid="audio-component" src={ previewUrl } controls>
+                        <track kind="captions" />
+                        O seu navegador não suporta o elemento
+                        <code>audio</code>
+                        .
+                      </audio>
+                      <input
+                        type="checkbox"
+                        data-testid={ `checkbox-music-${trackId}` }
+                        onClick={ onClick }
+                        className="songList"
+                        value={ trackId }
+                      />
+                    </div>
+                  )
+                  : null;
+              })
+            }
+          </div>
+        )
     );
   }
 }
